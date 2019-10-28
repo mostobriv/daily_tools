3c07fc9f584d929253663a253ded475938c4e1e3
mostobriv <15954313+mostobriv@users.noreply.github.com>
2019-10-28 13:48:09 +0700

    Update pwn.py

--- mylib/pwn.py
+++ mylib/pwn.py
@@ -1,13 +1,12 @@
-# it doesn't work actually, need to make it work (also mb changed ways this func communicates with sender/receiver)
+from os import system
+from pwn import Coredump
+from functools import wraps
 
+# it doesn't work actually, need to make it work (also mb changed ways this func communicates with sender/receiver)
 def padding_finder(path):
     def padding_finder_inner(func):
-        from functools import wraps
         @wraps(func)
         def wrapper(*args, **kwargs):
-            from os import system
-            from pwn import Coredump
-
             system('ulimit -c unlimited')
 
             func(*args, **kwargs)
