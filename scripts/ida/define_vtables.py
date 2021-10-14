e90820ce2c62acade5ebc224a20697345aa07aa2
mostobriv <mostobriv@gmail.com>
2021-10-14 18:30:12 +0700

    Added action to quick making of vtab

--- scripts/ida/define_vtables.py
+++ scripts/ida/define_vtables.py
@@ -11,9 +11,6 @@ idaapi.require('ida_strugle.util')
 from ida_strugle.virtual_table import VirtualTable
 from ida_strugle import const, util
 
-
-
-
 def define_all_vtables():
     segments = util.enum_all_segments()
     vtables_total = 0
@@ -52,3 +49,28 @@ def create_vtable(address):
         print("Imported: %s" % v.type_name)
     except:
         print("Got an exception!")
+
+
+class _MakeVirtualTable(idaapi.action_handler_t):
+    def __init__(self):
+        super(_MakeVirtualTable, self).__init__()
+        self.name           = "ida_strugle:MakeVirtualTable"
+        self.description    = "Create virtual table according to ida_strugle logic, at given address"
+        self.hotkey         = "Shift-V"
+
+    def activate(self, ctx):
+        create_vtable(here())
+
+    def update(self, ctx):
+        if ctx.widget_type == idaapi.BWN_DISASM:
+            return idaapi.AST_ENABLE_FOR_WIDGET
+        return idaapi.AST_DISABLE_FOR_WIDGET
+
+
+def __register_action(action):
+    result = idaapi.register_action(
+        idaapi.action_desc_t(action.name, action.description, action, action.hotkey)
+    )
+    print("Registered %s with status(%x)" % (action.name, result))    
+
+__register_action(_MakeVirtualTable())
